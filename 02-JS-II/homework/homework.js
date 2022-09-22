873029966413dca047cd3d88a76530c7e4a92feb
GAMUT <tavogusmejia@gmail.com>
2022-09-22 16:03:39 -0500

    Solution to the Homework

--- 02-JS-II/homework/homework.js
+++ 02-JS-II/homework/homework.js
@@ -7,100 +7,163 @@
 function devolverString(str) {
     // "Return" la string provista: str
     // Tu código:
+   return str;
   }
   
   function suma(x, y) {
     // "x" e "y" son números
     // Suma "x" e "y" juntos y devuelve el valor
     // Tu código:
+      var sumaXY=x+y;
+      return sumaXY;
   }
   
   function resta(x, y) {
     // Resta "y" de "x" y devuelve el valor
     // Tu código:
+    var restaXY= x - y;
+    return restaXY;
   }
   
   function multiplica(x, y) {
     // Multiplica "x" por "y" y devuelve el valor
     // Tu código:
+    var multiplicaXY=x*y;
+    return multiplicaXY;
   }
   
   function divide(x, y) {
     // Divide "x" entre "y" y devuelve el valor
     // Tu código:
+    var divisionXY=x/y;
+    return divisionXY;
+
   }
   
   function sonIguales(x, y) {
     // Devuelve "true" si "x" e "y" son iguales
     // De lo contrario, devuelve "false"
     // Tu código:
+    if(x===y){
+      return true;
+    }
+    else{
+      return false;
+    }
+
   }
   
   function tienenMismaLongitud(str1, str2) {
     // Devuelve "true" si las dos strings tienen la misma longitud
     // De lo contrario, devuelve "false"
     // Tu código:
+    if(str1.length===str2.length){
+      return true;
+    }
+    else{
+      return false;
+    }
   }
   
   function menosQueNoventa(num) {
     // Devuelve "true" si el argumento de la función "num" es menor que noventa
     // De lo contrario, devuelve "false"
     // Tu código:
+    if(num<90){
+      return true;
+    }
+    else{
+      return false;
+    }
+
   }
   
   function mayorQueCincuenta(num) {
     // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
     // De lo contrario, devuelve "false"
     // Tu código:
+    if(num>50){
+      return true;
+    }
+    else{
+      return false;
+    }
   }
   
   function obtenerResto(x, y) {
     // Obten el resto de la división de "x" entre "y"
     // Tu código:
+    var moduloXY= x%y;
+    return moduloXY;
   }
   
   function esPar(num) {
     // Devuelve "true" si "num" es par
     // De lo contrario, devuelve "false"
     // Tu código:
+    if(num%2===0){
+      return true;
+    }
+    else{
+      return false;
+    }
   }
   
   function esImpar(num) {
     // Devuelve "true" si "num" es impar
     // De lo contrario, devuelve "false"
     // Tu código:
+    if(num%2!==0){
+      return true;
+    }
+    else{
+      return false;
+    }
   }
   
   function elevarAlCuadrado(num) {
     // Devuelve el valor de "num" elevado al cuadrado
     // ojo: No es raiz cuadrada!
     // Tu código:
+    var potenciaCuadrada=num**2;
+    return potenciaCuadrada;
   }
   
   function elevarAlCubo(num) {
     // Devuelve el valor de "num" elevado al cubo
     // Tu código:
+    var potenciaCubo=Math.pow(num,3);
+    return potenciaCubo;
   }
   
   function elevar(num, exponent) {
     // Devuelve el valor de "num" elevado al exponente dado en "exponent"
     // Tu código:
+    var potencia=Math.pow(num,exponent);
+    return potencia;
   }
   
   function redondearNumero(num) {
     // Redondea "num" al entero más próximo y devuélvelo
     // Tu código:
+    var redondeaNumero = Math.round(num);
+    return redondeaNumero;
+ 
   }
   
   function redondearHaciaArriba(num) {
     // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
     // Tu código:
+    var redondeaNumero = Math.ceil(num);
+    return redondeaNumero;
   }
   
   function numeroRandom() {
     //Generar un número al azar entre 0 y 1 y devolverlo
     //Pista: investigá qué hace el método Math.random()
     // Tu código:
+    var numeroAleatorio = Math.random();
+    return numeroAleatorio;
   }
   
   function esPositivo(numero) {
@@ -109,45 +172,68 @@ function devolverString(str) {
     //Si el número es negativo, devolver ---> "Es negativo"
     //Si el número es 0, devuelve false
     // Tu código:
+    if(numero>0){
+      return "Es positivo";
+    }
+    else if(numero<0){
+      return "Es negativo";
+    }
+    else{
+    return false;
+    }
   }
   
   function agregarSimboloExclamacion(str) {
     // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
     // Ejemplo: "hello world" pasaría a ser "hello world!"
     // Tu código:
+    var newStr=str+"!";
+    return newStr;
   }
   
   function combinarNombres(nombre, apellido) {
     // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
     // Ejemplo: "Soy", "Henry" -> "Soy Henry"
     // Tu código:
+    var newName = nombre + " " + apellido;
+    return newName;
   }
   
   function obtenerSaludo(nombre) {
     // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
     // "Martin" -> "Hola Martin!"
     // Tu código:
+    return "Hola "+nombre+"!";
+
   }
   
   function obtenerAreaRectangulo(alto, ancho) {
     // Retornar el area de un rectángulo teniendo su altura y ancho
     // Tu código:
+    var newAreaRec=alto*ancho;
+    return newAreaRec;
   }
   
   function retornarPerimetro(lado) {
     //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
     //Escribe tu código aquí
+    var newSquarePerimeter=4*lado;
+    return newSquarePerimeter;
+  
   }
   
   function areaDelTriangulo(base, altura) {
     //Desarrolle una función que calcule el área de un triángulo.
     //Escribe tu código aquí
+    return base*altura/2;
   }
   
   function deEuroAdolar(euro) {
     //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
     //como parámetro un número de euros y calcule el cambio en dólares.
     //Escribe tu código aquí
+    var eurosToDollar = euro*1.20;
+    return eurosToDollar;
   }
   
   // No modificar nada debajo de esta línea
--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,57 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    var sumaValores=0;
+    for(let x=1; x<=10; x++){
+    sumaValores=sumaValores+x;
+    }
+return sumaValores;
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  var valoresPares=[];
+
+    for(let x=0; x<array.length; x++){
+      if(array[x]%2===0){
+        valoresPares.push(array[x]);
+      }
+    }
+return valoresPares;
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
-}
+  //var valoresCuadrados=[];
+  //var x=0;
+  //while (x<array.length){
+//valoresCuadrados.push(Math.pow(array[x],2));
+  //  x++; 
+//}return valoresCuadrados;
+ var arrayAlCuadrado = array.map(function(valorElemento){return valorElemento ** 2;})
+return arrayAlCuadrado;
+  }
+  
+  
+
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
-}
+  var arraySumado = array.reduce(function(total,elemento){return total + elemento;})
+  return arraySumado;
+    }
+
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  return num.toString().length;
 }
   
   
